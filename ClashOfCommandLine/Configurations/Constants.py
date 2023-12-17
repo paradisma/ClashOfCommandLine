fdc169ed0172d3d2874fda128c61c55c78714a9a
mparadis <mparadis19@gmail.com>
2023-12-17 17:29:08 -0500

    Adding beginning changes
    Adding character ascii images
    
    prev commit cont.

---
+++ ClashOfCommandLine/Configurations/Constants.py
@@ -0,0 +1,16 @@
+# The factor in which a character will need to level up as they progress.
+EXPERIENCE_TO_NEXT_LEVEL_FACTOR = 1.25
+
+# The factor in which a character's health will level up as they progress.
+HEALTH_LEVEL_UP_FACTOR = 1.3
+
+# The factor in which a character's stamina will level up as they progress.
+STAMINA_LEVEL_UP_FACTOR = 1.3
+
+# The default ascii character representation
+DEFAULT_CHARACTER_REPRESENTATION = (
+"""+-----+
+|  X  |
+| X X |
+|  X  |
++-----+""")
---
+++ ClashOfCommandLine/Core/Enums/FighterType.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+class FighterType(Enum):
+    FIRE = 1
+    EARTH = 2
+    WATER = 3
+    AIR = 4
